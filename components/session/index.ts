7e2ab83310001ecb921c25dd8f8d699867db571e
marcksdbgg <geraldo.romero@ucsp.edu.pe>
2025-12-09 11:46:48 -0500

    feat: Export various UI and session components through new `index.tsx` files.

--- components/session/index.ts
+++
@@ -1,7 +0,0 @@
-// Session Components
-export { default as SmartTextRenderer } from './SmartTextRenderer';
-export { default as EditableList } from './EditableList';
-export { default as SectionHeader } from './SectionHeader';
-export { default as SessionSection } from './SessionSection';
-export { default as SessionNavbar, EditingBanner, MobileActionButtons } from './SessionNavbar';
-export { default as FichaCard } from './FichaCard';
--- components/ui/index.ts
+++
@@ -1,3 +0,0 @@
-// UI Components
-export { default as Tooltip } from './Tooltip';
-export { default as ResourceButton, ImageButton, PendingResourceButton } from './ResourceButton';
